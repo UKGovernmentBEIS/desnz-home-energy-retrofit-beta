776c7af6007f826fb24b4e68ec92584e3e435a41
Glenn Clarke <glenn.clarke@softwire.com>
2024-07-25 15:12:07 +0100

    PC-1024: Add migration to delete Sessions contents

---
+++ HerPublicWebsite.Data/Migrations/20240725140407_DeleteSessionsContents.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+#nullable disable
+
+namespace HerPublicWebsite.Data.Migrations
+{
+    public partial class DeleteSessionsContents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Operations.Add(new SqlOperation
+            {
+                Sql = "TRUNCATE TABLE \"Sessions\""
+            });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
